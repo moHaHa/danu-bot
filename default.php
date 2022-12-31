9f74785d3bee4cd5e6ada41170aa3a8f9d1bd372
Mohammad ْAl Hallaq <72744132+HBO30@users.noreply.github.com>
2022-12-31 10:39:13 +0300

    Create default.php

---
+++ default.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Telegram webhook bot Example.
+ * @author Egor Egorov <egor@erorrov.ru>
+ */
+
+//Include file (class)
+
+require_once __DIR__ . '/bot.php';
+//Instances class
+$tg = new Telegram("5810952608:AAFM59Pft9aoh6bJHLYEzedPFKAriA12mAg");
+
+//Get an array of an incoming update
+$result = $tg->getData();
+$text = $result['message']['text']; 
+$chatID = $result['message']['chat']['id'];
+
+//First command
+if($text == "/start"){
+  //Build buttons
+  $btn1 = $tg->buildReplyButton(["text" => "Ping"]);
+  $btn2 = $tg->buildReplyButton(["text" => "🌅"]);
+  $btn3 = $tg->buildReplyButton(["text" => "Close keyboard"]);
+  $btn4 = $tg->buildReplyButton(["text" => "Help"]);
+
+  //Position of buttons on keyboard
+  $buttons = [
+    [$btn1, $btn2],
+    [$btn4],
+    [$btn3]
+  ];
+
+  //Build keyboard
+  $rm = $tg->buildReplyKeyboard($buttons);
+
+  //Send a message with keyboard (reply_markup)
+  $params = ["chat_id" => $chatID, "text" => "Hey. What do you want?", "reply_markup" => $rm];
+  $tg->sendRequest("sendMessage", $params);
+}
+
+//Send simple text messages
+if($text == "Ping"){
+  $params = ["chat_id" => $chatID, "text" => "Pong"];
+  $tg->sendRequest("sendMessage", $params);
+}
+
+//Send image
+if($text == "🌅"){
+  //Load file (loadFile supports any file extension)
+  $img = $tg->loadFile("test.png");
+  //Sending a message with $img as photo
+  $params = ["chat_id" => $chatID, "photo" => $img, "caption" => "This is a simple example how to send a picture"];
+  $tg->sendRequest("sendPhoto", $params);
+}
+
+//Closing keyboard
+if($text == "Close keyboard"){
+  $rm = $tg->removeReplyKeyboard();
+  $params = ["chat_id" => $chatID, "text" => "Send /start to see keyboard again", "reply_markup" => $rm];
+  $tg->sendRequest("sendMessage", $params);
+}
+
+//Message with an inline keyboard
+if($text == "Help"){
+  //Build buttons (https://core.telegram.org/bots/api#inlinekeyboardbutton)
+  $btn1 = $tg->buildInlineButton(["text" => "📄 View on GitHub", "url" => "https://github.com/HBO30"]);
+  $btn2 = $tg->buildInlineButton(["text" => "Author", "url" => "https://t.me/edthre"]);
+
+  //Position of buttons
+  $buttons = [
+    [$btn1],
+    [$btn2]
+  ];
+
+  //Build keyboard
+  $rm = $tg->buildInlineKeyboard($buttons);
+
+  //Send a message with an inline keyboard (reply_markup)
+  $params = ["chat_id" => $chatID, "text" => "This bot is created by simple-telegram class", "reply_markup" => $rm];
+  $tg->sendRequest("sendMessage", $params);
+}
+?>
